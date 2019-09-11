8c0d16f820e9df686f9c96369d7091ec3bd472f4
sameesh-s <sameesh.fisat@gmail.com>
2019-09-11 18:38:02 +0530

    Intilizing mongo maven project

---
+++ spring_mongo_example/src/test/java/com/sameesh/spring_mongo_example/SpringMongoExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sameesh.spring_mongo_example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMongoExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
