1a93bfcef7ab73d154eb541198f7f164b8c2cb47
sameesh-s <sameesh.fisat@gmail.com>
2019-09-14 18:45:03 +0530

    Logging with sl4j

--- spring_mongo_example/src/main/java/com/sameesh/spring_mongo_example/restcontroller/StatusController.java
+++ spring_mongo_example/src/main/java/com/sameesh/spring_mongo_example/restcontroller/StatusController.java
@@ -2,6 +2,8 @@ package com.sameesh.spring_mongo_example.restcontroller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,9 +18,18 @@ public class StatusController {
 	@Autowired
 	private StatsService statsService;
 	
+	Logger logger = LoggerFactory.getLogger(StatusController.class);
+
+	
 	//*******************methods for application ********************************
 	@RequestMapping(value ="/applications", method=RequestMethod.GET)
 	public List<String> getApplicationsStat(){
+		
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
 		System.out.println("Application stat is controller hit::");
 		statsService.createStat();
 		return null;
